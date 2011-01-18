43a5f451ef8cc944349cb7babec04b8e9bd9fdac
Kenichi Ishigaki <ishigaki@cpan.org>
2011-01-18 23:10:33 +0900

    releng

--- lib/HTML/Filter/Callbacks.pm
+++ lib/HTML/Filter/Callbacks.pm
@@ -5,7 +5,7 @@ use warnings;
 use base 'HTML::Parser';
 use HTML::Filter::Callbacks::Tag;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 my %Handlers = (
   start => [\&_handler, 'self,event,tokens,text,skipped_text'],
