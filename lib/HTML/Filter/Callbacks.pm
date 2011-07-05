2b0b17dca7e70a3d2f77f7c1b7f4baad41b6528d
xaicron <xaicron@gmail.com>
2011-07-06 00:32:22 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e8a6ce5..e37224d 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for HTML-Filter-Callbacks
    
    +0.07 2011/07/06
    +  - fixed broken dist
    +  - ignore cases of remove_attr and replace_attr (xaicron)
    +
     0.06 2011/07/05
       - ignore cases fo attr (xaicron)

--- lib/HTML/Filter/Callbacks.pm
+++ lib/HTML/Filter/Callbacks.pm
@@ -5,7 +5,7 @@ use warnings;
 use base 'HTML::Parser';
 use HTML::Filter::Callbacks::Tag;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 my %Handlers = (
   start => [\&_handler, 'self,event,tokens,text,skipped_text'],
